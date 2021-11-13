86839ac915b157bd4ec6f42606466ddc1203f975
Dom Whooley <dom.whooley.casual@guardian.co.uk>
2021-11-13 18:00:22 +0000

    version 2 breaking apart the original function and rudimentary observer

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,39 +1,155 @@
 // if you want to import a module from shared/js then you can
 // just do e.g. import Scatter from "shared/js/scatter.js"
 
-// mainContent
+// get our content contaciner
 const mainContent = document.getElementById('maincontent')
-// get all H2s
+// get our target element
 const targetElem = mainContent.querySelectorAll('h2')
-// console.log(targetElem)
 
 // get first set of links
-const firstList = mainContent.querySelectorAll('ul')[0] // only needed if menu list exists
+const firstList = mainContent.querySelectorAll('ul')[0] // ********* only needed if menu list exists
 
-function wrapElem(tagName,idName,className,targetContainer) {
-  // build wrappers add content
-  const newList = document.createElement(tagName);
-  newList.setAttribute('id', idName);
-  newList.classList.add(className);
-  targetContainer.prepend(newList)
+//////////////////////////////////////////////////////////////////////////
+// ---------------------------- Version 2 ----------------------------- //
+//////////////////////////////////////////////////////////////////////////
+
+const navID             = 'jump-nav';
+const anchorTag         = 'header';
+const navTag            = 'nav';
+const anchorIdLabel     = 'section';
+const anchorClass       = 'section-header';
+const navClass          = 'article-navigation';
+const navBarClass       = 'nav-title-wrapper';
+const navUtilityID      = 'nav-utility';
+
+// creates elements with attributes and adds them *NB Needs refinement?
+function newElem(tagName,attType,attName,className,content,contentHolder,position) {
+
+  const newElem2 = document.createElement(tagName);
+
+  if (className !== '' ) {
+    newElem2.classList.add(className);
+  }
+
+  newElem2.innerText = content;
+
+  if (attType !== '' ) {
+    newElem2.setAttribute(attType, attName);
+  }
+  // Not 100% sure about this part?
+  if (position === 'before') {
+    contentHolder.prepend(newElem2);
+  } else if ( position === 'after') {
+    contentHolder.appendChild(newElem2);
+  }
+}
+
+// wraps an element
+function wrapElem(el, wrapper) {
+    el.parentNode.insertBefore(wrapper, el);
+    wrapper.appendChild(el);
+}
+
+// nav menu builder
+function menuContainer() {
+  // build nav
+  newElem(navTag,'id',navID, navClass,'',mainContent,'before');
+  const menuHolder = document.getElementById(navID);
+
+  newElem('div','id',navUtilityID,navBarClass,'',menuHolder,'before');
+  const navUtility = document.getElementById(navUtilityID);
+
+  newElem('p','','','','Jump to',navUtility,'after');
+  newElem('span','','','','Close',navUtility,'after');
 
 }
 
-// wrapElem('div','new-list2','article-navigation',mainContent);
+menuContainer(); // ******* Building this now because it's needed to add the links, but should be when *no* nav has been found
+const navHolder = document.getElementById(navID); // added #2 to not conflict
 
+// wrap our anchors
+function addAnchorWrap(targetElem, i) {
+  let index = parseInt(i);// change string to interger to start at 1
+  const anchorNode = targetElem[i];
+  const anchorID = anchorIdLabel + (index+1);
+  // create header element
+  const titleWrapper = document.createElement(anchorTag);
+  titleWrapper.setAttribute('id', anchorID);
+  titleWrapper.classList.add(anchorClass);
+  // wrap our header element
+  wrapElem(anchorNode, titleWrapper);
+  // Add marker *if* we need it?
+  // newElem('span','','','marker','',titleWrapper,'before');
+}
+
+// Builds our url's. Currently the whole link but may need just urls output
+function linkURL(targetElem, i) {
+  let index = parseInt(i); // change string to interger to start at 1
+  const anchorID = anchorIdLabel + (index+1);
+  const linkTitle = targetElem[i].innerText;
+  const linkHref = '#' + anchorID;
+  newElem('a','href',linkHref,'nav-class',linkTitle,navHolder,'after');
+}
+
+// Loop through and build anchors and menu links
+for (let i = 0; i < targetElem.length; i++) {
+  // wrap anchors
+  addAnchorWrap(targetElem, [i]);
+  // build link
+  linkURL(targetElem, [i]);
+}
+
+// detect section in view
+let sectionHeader = document.querySelectorAll(anchorTag);
+
+let n=0;
+let observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      console.log('Section ' + `${++n}` + ' in view ');
+      observer.unobserve(entry.target);
+    }
+  });
+}, {threshold: 1});
+
+sectionHeader.forEach(header => { observer.observe(header) });
+
+// ---------------------------- Detect menu ----------------------------- //
+
+// check for ul menu
+// var getNextSibling = function (elem, selector) {
+//
+// 	// Get the next sibling element
+// 	var sibling = elem.nextElementSibling;
+//
+// 	// If the sibling matches our selector, use it
+// 	// If not, jump to the next sibling and continue the loop
+// 	while (sibling) {
+// 		if (sibling.matches(selector)) return sibling;
+// 		sibling = sibling.nextElementSibling
+// 	}
+//
+// };
+// var findList = document.querySelector('ul');
+// if () {
+//   content.querySelector('ul').innerHTML
+// }
+
+//////////////////////////////////////////////////////////////////////////
+// ---------------------------- Version 1 ----------------------------- //
+//////////////////////////////////////////////////////////////////////////
+
+// addNewList(); // inactive version
 function addNewList() {
     // build navigation container
-    const newList = document.createElement("nav")
-    newList.setAttribute("id", "new-list")
-    newList.classList.add("article-navigation")
-    mainContent.prepend(newList)
-
-    // console.log(wrapElem('div','new-list2','article-navigation',mainContent));
-
+    const navHolder = document.createElement("nav")
+    navHolder.setAttribute("id", "new-list")
+    navHolder.classList.add("article-navigation")
+    mainContent.prepend(navHolder)
     // Create nav wrapper
     const navTitleWrapper = document.createElement('div')
     navTitleWrapper.classList.add("nav-title-wrapper")
-    newList.appendChild(navTitleWrapper)
+    navHolder.appendChild(navTitleWrapper)
     // create nav title and close button
     const navTitle = document.createElement('p')
     const navChevron = document.createElement('span')
@@ -44,33 +160,31 @@ function addNewList() {
     navTitleWrapper.appendChild(navChevron)
 
     for (var i = 0; i < targetElem.length; i++) {
-        var node = targetElem[i];
-        // create new div
-        const titleWrapper = document.createElement('div')
-        titleWrapper.setAttribute('id', 'nav' + i)
+        const anchorNode = targetElem[i];
+        const anchorID = 'nav' + i;
+
+        const titleWrapper = document.createElement('header');
+        titleWrapper.setAttribute('id', anchorID);
+
         const marker = document.createElement('span')
         marker.classList.add("marker")
         titleWrapper.appendChild(marker)
         console.log(marker)
-        // add new div
-        node.parentNode.insertBefore(titleWrapper, node);
-        // remove old H2
-        node.parentNode.removeChild(node);
-        // add H2 into div
-        titleWrapper.appendChild(node);
-        console.log(node)
 
+        // add new div
+        anchorNode.parentNode.insertBefore(titleWrapper, anchorNode);
+        // remove old H2
+        anchorNode.parentNode.removeChild(anchorNode);
+        // add H2 into div
+        titleWrapper.appendChild(anchorNode);
+        console.log(anchorNode)
         // create new nav links
-        var navLink = document.createElement('a');
+        const navLink = document.createElement('a');
         // add hrefs to new links
         navLink.href = '#' + titleWrapper.getAttribute('id');
         // get h2 text and add to new links
         navLink.innerHTML = targetElem[i].innerHTML
         // add new links to nav list
-        newList.appendChild(navLink)
-
+        navHolder.appendChild(navLink)
     }
-
-
 }
-addNewList()
