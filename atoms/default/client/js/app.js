2053de881f51fc7acd003ae3ba2134abd21471ba
Dom Whooley <dom.whooley.casual@guardian.co.uk>
2021-11-18 17:06:28 +0000

    udated loop and text anchors

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,24 +1,28 @@
-// get our content contaciner
-const mainContent = document.getElementById('maincontent')
-// get our target element
-const targetElem = mainContent.querySelectorAll('h2')
 
+const navID             = 'jump-nav';
+const anchorTag         = 'header';
+const targetTag         = 'h2';
+const targetTagInner    = 'strong';
+const navTag            = 'nav';
+const menuClass         = 'nav-class';
+const anchorIdLabel     = 'section';
+// const anchorIdLabel     = null;
+const anchorClass       = 'section-header';
+const navClass          = 'article-navigation';
+const navBarClass       = 'nav-title-wrapper';
+const navUtilityID      = 'nav-utility';
+
+// get our content contaciner
+const mainContent = document.getElementById('maincontent');
+// get our target element
+const targetElem = mainContent.querySelectorAll(targetTag);
+// const targetDef = mainContent.querySelector('strong');
 // get first set of links
 const firstList = mainContent.querySelectorAll('ul')[0] // ********* only needed if menu list exists
 
 //////////////////////////////////////////////////////////////////////////
 // ---------------------------- Version 2 ----------------------------- //
 //////////////////////////////////////////////////////////////////////////
-
-const navID             = 'jump-nav';
-const anchorTag         = 'header';
-const navTag            = 'nav';
-const menuClass          = 'nav-class';
-const anchorIdLabel     = 'section';
-const anchorClass       = 'section-header';
-const navClass          = 'article-navigation';
-const navBarClass       = 'nav-title-wrapper';
-const navUtilityID      = 'nav-utility';
 
 // creates elements with attributes and adds them *NB Needs refinement?
 function newElem(tagName,attType,attName,className,content,contentHolder,position) {
@@ -68,14 +72,25 @@ const navHolder = document.getElementById(navID); // added #2 to not conflict
 // wrap our anchors
 function addAnchorWrap(targetElem, i) {
   let index = parseInt(i);// change string to interger to start at 1
-  const anchorNode = targetElem[i];
-  const anchorID = anchorIdLabel + (index+1);
+  const anchorNode      = targetElem[i];
+  const anchorID        = anchorIdLabel + (index+1);
+  const linkTitle       = targetElem[i].innerText;
+  const anchorIDTitle   = linkTitle.replace(/\s+/g, '-').toLowerCase();
+  // console.log(anchorIDTitle);
   // create header element
   const titleWrapper = document.createElement(anchorTag);
-  titleWrapper.setAttribute('id', anchorID);
+
+  // titleWrapper.setAttribute('id', anchorID); // using section var
+  titleWrapper.setAttribute('id', anchorIDTitle); // using anchor text
+
   titleWrapper.classList.add(anchorClass);
-  // wrap our header element
-  wrapElem(anchorNode, titleWrapper);
+
+  const innerNode = anchorNode.querySelector(targetTagInner); // contains targetTagInner tag
+
+  if (anchorNode.contains(innerNode)) {
+    // wrap our header element only if it contains our inner node
+    wrapElem(anchorNode, titleWrapper);
+  }
   // Add marker *if* we need it?
   // newElem('span','','','marker','',titleWrapper,'before');
 }
@@ -83,10 +98,19 @@ function addAnchorWrap(targetElem, i) {
 // Builds our url's. Currently the whole link but may need just urls output
 function linkURL(targetElem, i) {
   let index = parseInt(i); // change string to interger to start at 1
-  const anchorID = anchorIdLabel + (index+1);
-  const linkTitle = targetElem[i].innerText;
-  const linkHref = '#' + anchorID;
-  newElem('a','href',linkHref,menuClass,linkTitle,navHolder,'after');
+  const anchorNode      = targetElem[i];
+  const anchorID        = anchorIdLabel + (index+1);
+  const linkTitle       = targetElem[i].innerText;
+  const anchorIDTitle   = linkTitle.replace(/\s+/g, '-').toLowerCase();
+
+  // const linkHref = '#' + anchorID; // using section variable
+  const linkHref = '#' + anchorIDTitle; // using anchor text
+
+  const innerNode = anchorNode.querySelector(targetTagInner); // contains targetTagInner tag
+
+  if (anchorNode.contains(innerNode)) {
+    newElem('a','href',linkHref,menuClass,linkTitle,navHolder,'after');
+  }
 }
 
 // Loop through and build anchors and menu links
@@ -100,32 +124,43 @@ for (let i = 0; i < targetElem.length; i++) {
 // detect section in view
 let sectionHeader = document.querySelectorAll(anchorTag);
 
-// console.log(sectionHeader)
-
-
 const menuTarget = document.getElementsByClassName(menuClass);
+const menuTargetArr = [].slice.call(menuTarget); // create an array for forEach in safari
+
+console.log(menuTargetArr);
 let currItem = null;
 let prevItem = null;
 let options = {
-  rootMargin: '0px 0px -75% 0px', // add for sticky menu
+  rootMargin: '0px 0px -75% 0px', // add for sticky menu reduce target area to top 25% of viewport for small sections
   threshold: 1.0
 }
+
 let observer = new IntersectionObserver((entries, observer) => {
   entries.forEach(entry => {
     if(entry.isIntersecting){
-      const labelText = entry.target.innerText;
+      const labelText = entry.target.textContent; // changed innerText to text Content to work in safari
       // previous is current
         prevItem = currItem;
-        console.log('prevItem: ' + prevItem)
-        menuTarget.forEach(function(item){
-          if (labelText === item.innerText) {
-            console.log(item)
-            item.classList.add('active')
+
+        menuTargetArr.forEach(function(item){
+          // loop through links to match active target text
+          if ( labelText === item.textContent ) {
+
+            item.classList.add('active');
             // get current item
             currItem = labelText;
-            console.log('currItem: ' + currItem);
+            // console.log('currItem: ' + currItem);
+
+          } else if ( prevItem === item.textContent ) {
+
+            item.classList.add('prev');
+            // console.log('prevItem: ' + prevItem);
+
           } else {
-            item.classList.remove('active')
+
+            item.classList.remove('active');
+            item.classList.remove('prev');
+
           }
         });
       }
